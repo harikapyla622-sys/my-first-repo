859d677ccdfd9f51ef40301e51189bdceb095a06
Harika Pyla <harikapyla622@gmail.com>
2026-06-05 15:02:44 -0500

    Retreive sample.txt and add error handling

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-with open('sample.txt') as file:
-    content = file.read()
+try:
+    with open('sample.txt') as file:
+        content = file.read()
 
-#content_capitalized = content.upper()
-print("File content: ")
-#print(content_capitalized)
-print(content)
-print("Done")
+    #content_capitalized = content.upper()
+    print("File content: ")
+    #print(content_capitalized)
+    print(content)
+    print("Done")
+except FileNotFoundError:
+    print("File not found.")

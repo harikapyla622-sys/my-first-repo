3d004e8069a0eafd1421e1471070c3bafcad87d7
Harika Pyla <harikapyla622@gmail.com>
2026-06-05 14:11:47 -0500

    removed the capitalized and gave only the print content

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 with open('sample.txt') as file:
     content = file.read()
 
-content_capitalized = content.upper()
+#content_capitalized = content.upper()
 print("File content: ")
-print(content_capitalized)
+#print(content_capitalized)
+print(content)
 print("Done")

df3dc187b736b384e688d4fea85b304ad51c44a6
Harika Pyla <harikapyla622@gmail.com>
2026-06-05 22:57:56 -0500

    Added welcome as a new featire to the application

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-<<<<<<< HEAD
+
 try:
     with open('sample.txt') as file:
         content = file.read()
 
     #content_capitalized = content.upper()
+    print("Welcome to the File Manager Application!")
     print("File content: ")
     #print(content_capitalized)
     print(content)
     print("Done")
+
+
+    user_input = input("Enter some text: ")
+
+        
+        
+    with open("user_file.txt", "w") as file:
+        file.write("user_input: " + user_input)
+    
+
+
 except FileNotFoundError:
     print("File not found.")
-=======
-user_input = input("Enter some text: ")
-
-with open('sample.txt') as file:
-    content = file.read()
-    
-    
-with open("user_file.txt", "w") as file:
-    file.write("user_input: " + user_input)
-    
-    
-#content_capitalized = content.upper()
-print("File content: ")
-#print(content_capitalized)
-print(content)
-print("Done")
->>>>>>> feature/write-capability
